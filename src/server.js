3d0321863b90290668ba684619e0017a53ac3bea
wikomoko <mokoindonesia45@gmail.com>
2023-03-27 22:43:07 +0700

    coba ubah

--- src/server.js
+++ src/server.js
@@ -1,21 +1,32 @@
-const Hapi = require("@hapi/hapi");
-const routes = require("./routes");
+// const Hapi = require("@hapi/hapi");
+// const routes = require("./routes");
 
-const init = async () => {
-  const server = Hapi.server({
-    port: 5000,
-    host: "0.0.0.0",
-    routes: {
-      cors: {
-        origin: ["*"],
-      },
-    },
-  });
+// const init = async () => {
+//   const server = Hapi.server({
+//     port: 5000,
+//     host: "0.0.0.0",
+//     routes: {
+//       cors: {
+//         origin: ["*"],
+//       },
+//     },
+//   });
 
-  server.route(routes);
+//   server.route(routes);
 
-  await server.start();
-  console.log(`Server berjalan pada ${server.info.uri}`);
-};
+//   await server.start();
+//   console.log(`Server berjalan pada ${server.info.uri}`);
+// };
 
-init();
+// init();
+const express = require('express')
+https://runkit.com/const app = express()
+const port = 5000
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
